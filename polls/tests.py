b531afcdc86fa3c15517747bfee6679622573b19
Francis Tsang <frtsang40@gmail.com>
2020-06-02 09:44:09 -0700

    finished part 6

--- polls/tests.py
+++ polls/tests.py
@@ -124,3 +124,7 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+
+
+    
